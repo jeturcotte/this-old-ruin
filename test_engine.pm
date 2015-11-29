c8299ace64c1b9c2564bbad182b954dc872f1b02
jeturcotte <joshua.eric.turcotte@gmail.com>
2015-11-28 22:10:59 -0500

    beginning to sort engine into rudiemntary game logic

--- test_engine.pm
+++ test_engine.pm
@@ -1,10 +1,21 @@
 use v6;
 
-react {
-    whenever IO::Socket::Async.listen('localhost',3333) -> $conn {
-        say "connection established";
-	my $bs = $conn.bytes-supply;
-        $bs.tap(-> $buf { await $conn.write: $buf } );
-        $bs.wait;
-    }
+while (1) {
+  get_player_commands();
+  calculate_game_events();
+  report_events_to_player();  
 }
+
+sub get_player_commands {
+ react {
+    whenever IO::Socket::Async.listen('localhost',3333) -> $conn {
+      my $bs = $conn.bytes-supply;
+      $bs.tap(-> $buf { await $conn.write: $buf } );
+      $bs.wait;
+    }
+  }
+}
+
+sub calculate_game_events { }
+
+sub report_events_to_player { }
