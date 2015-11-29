58fbd0bffc2bd0c1e9a8a690b13a3140cdab794e
jeturcotte <joshua.eric.turcotte@gmail.com>
2015-11-28 21:47:09 -0500

    only just beginning to explore the engine example

--- test_engine.pm
+++ test_engine.pm
@@ -2,9 +2,9 @@ use v6;
 
 react {
     whenever IO::Socket::Async.listen('localhost',3333) -> $conn {
-        my $bs = $conn.bytes-supply;
+        say "connection established";
+	my $bs = $conn.bytes-supply;
         $bs.tap(-> $buf { await $conn.write: $buf } );
         $bs.wait;
-        $conn.close;
     }
 }
