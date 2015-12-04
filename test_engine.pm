f9408097c396ba0b85f14d89d16dfd57ec95335e
jeturcotte <joshua.eric.turcotte@gmail.com>
2015-12-03 21:04:13 -0500

    Whaddayaknow

--- test_engine.pm
+++ test_engine.pm
@@ -1,11 +1,13 @@
 use v6;
 
-get_player_commands(); 
+start get_player_commands(); 
 our $target_tics_per_second = 30;
+our $game_hour = 60 * 60 * $target_tics_per_second; # 24 'days' per real day, or about a day per hour in game time, in tics
 our $maximum_wait_time = 1000 / $target_tics_per_second;
 our $begin_time;
 our $time_elapsed;
 our $tic_leeway;
+our $which_tic = 0;
 
 while (1) {
   $begin_time = DateTime.now().Instant; 
@@ -13,8 +15,8 @@ while (1) {
   calculate_game_events();
   report_events_to_player();
   my $time_elapsed = (DateTime.now().Instant - $begin_time) * 1000;  
-  my $tic_leeway = ($time_elapsed / $maximum_wait_time) * 100;
-  say "game tic elapsed with a process usage of $tic_leeway\% ($time_elapsed milliseconds used out of $maximum_wait_time)";
+  #my $tic_leeway = ($time_elapsed / $maximum_wait_time) * 100;
+  #say "game tic elapsed with a process usage of $tic_leeway\% ($time_elapsed milliseconds used out of $maximum_wait_time)";
   sleep(($maximum_wait_time - $time_elapsed)/1000);
 }
 
@@ -23,6 +25,7 @@ sub get_player_commands {
     whenever IO::Socket::Async.listen('localhost',3333) -> $conn {
       my $bs = $conn.bytes-supply;
       $bs.tap(-> $buf { await $conn.write: $buf } );
+      say 'player command received';
       $bs.wait;
     }
   }
@@ -36,6 +39,7 @@ sub process_player_commands {
 }
 
 sub calculate_game_events {
+  determine_if_game_hour_has_passed(); 
   # process ongoing registered incidents
   # process creature movement request
   # process climate change request  
@@ -50,4 +54,15 @@ sub report_events_to_player {
     # report important climate information
     # report alteration in climate information
   # otherwise, report failure to move to new room
+}
+
+sub determine_if_game_hour_has_passed {
+  $which_tic++;
+  unless $which_tic % 300 {
+    say '10 tics have passed';
+  }
+  if $which_tic == $game_hour {
+    $which_tic = 0;
+    say 'A game hour has passed';
+  }
 }
