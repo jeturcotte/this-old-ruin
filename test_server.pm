ba7ae8ce175f0fa6970fc364ea589235c19d110d
jeturcotte <joshua.eric.turcotte@gmail.com>
2015-12-07 20:31:32 -0500

    still trying to get comfortable with the basics

---
+++ test_server.pm
@@ -0,0 +1,16 @@
+use v6;
+
+my $server = IO::Socket::INET.new(
+  :localport(7041),
+  :type(1),
+  :reuse(1),
+  :listen(10)
+);
+
+loop {
+  my $connection = $server.accept;
+  while my $inc = $connection.recv(:bin) {
+    say "heard $inc";
+    $connection.write: 'ok!\n';
+  }
+}
