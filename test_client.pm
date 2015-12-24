7082ccdfcd879cf7b19243dd8188e49f6a637b3d
jeturcotte <joshua.eric.turcotte@gmail.com>
2015-12-24 20:28:49 +0000

    catching client up a little bit

--- test_client.pm
+++ test_client.pm
@@ -2,17 +2,16 @@ use v6;
 
 # TODO -- Pull the following from config instead
 our $port = 7041;
-#push our @commands, @*ARGS.perl || 'No command given';
-#say "commands given: {@commands}";
+push our @commands, @*ARGS.perl || 'No command given';
+say "commands given: {@commands}";
 # TODO -- and/or accept command line arguments as override
 
 await IO::Socket::Async.connect('localhost',$port).then( -> $p {
     if $p.status {
-        #my $msg = prompt(' >');
         given $p.result {
-            .print('whee');
+            .print(@commands);
             react {
-                whenever .chars-supply() -> $v {
+                whenever .Supply() -> $v {
                     $v.say;
                     done;
                 }
