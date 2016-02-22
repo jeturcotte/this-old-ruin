74af10e63bcf0a56cfed29d709155cfb181498a6
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-02-21 22:13:45 -0500

    dont think i want switches

--- p6lib/Game/Commands.pm
+++ p6lib/Game/Commands.pm
@@ -6,8 +6,21 @@ class Game::Commands {
     return $command.decode('UTF-8').chomp.uc;
   }
 
+  # I feel like this will mutate into the event class in the adventure thing by masak
+  # let's at least try to do it different at first
+  # problem being, I'd rather have methods attached to each primary command
+  # rather than a switch case... 
+  # further, it has no access here to game information... so thats going to change soon
   method Respond($command) {
-    return $command ~ "!\n";   
+
+    my @particles = $command.split(" ");
+    say "interpreting: " ~ @particles.join(", ");
+    given @particles[0] {
+      when "WHO" { return "PFF.  WHO?  Mind your business!\n"; }
+      when "LOOK" { return "Well... you looked.  Nuff Said!\n"; }
+      default { return "Huh?\n";i }
+    }    
+
   }
 
 } 
