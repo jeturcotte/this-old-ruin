df6ce2befbaeeb6ee0cfd3876f981f926e30532d
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-03-12 22:04:06 -0500

    not as sure about this

--- p6lib/Game.pm
+++ p6lib/Game.pm
@@ -10,6 +10,21 @@ class Game {
   has $!out = Supplier.new;
   has $!outgoing = $out.Supply;
 
+	method tick {
+		
+		while (1) {
+    	$begin_time = DateTime.now().Instant; 
+    	process_player_commands();
+    	calculate_game_events();
+    	report_events_to_player();
+    	my $time_elapsed = (DateTime.now().Instant - $begin_time) * 1000;  
+    	#my $tic_leeway = ($time_elapsed / $maximum_wait_time) * 100;
+    	#say "game tic elapsed with a process usage of $tic_leeway\% ($time_elapsed milliseconds used out of $maximum_wait_time)";
+    	sleep( ($maximum_wait_time_in_milliseconds - $time_elapsed) / 1000 );
+  	}
+
+	}
+
   method communicator {
 
     react {
--- test_engine.pm
+++ test_engine.pm
@@ -26,48 +26,7 @@ use Game;
   our $interpreter = Game::Commands.new;
   start accept_player_commands();
 
-  while (1) {
-    $begin_time = DateTime.now().Instant; 
-    process_player_commands();
-    calculate_game_events();
-    report_events_to_player();
-    my $time_elapsed = (DateTime.now().Instant - $begin_time) * 1000;  
-    #my $tic_leeway = ($time_elapsed / $maximum_wait_time) * 100;
-    #say "game tic elapsed with a process usage of $tic_leeway\% ($time_elapsed milliseconds used out of $maximum_wait_time)";
-    sleep( ($maximum_wait_time_in_milliseconds - $time_elapsed) / 1000 );
-  }
-# END GAME LOOP
-
-
-
-# CLIENT CONNECTION AND INTERFACE
-  sub accept_player_commands {
-    react {
-
-    # ESTABLISH CONNECTION WITH CLIENT
-      whenever Game::Socket.listen('localhost',$port) -> $conn {
-        $conn.write: "welcome; the server dubs thee $conn.name()\n".encode();
-        %client_connections{$conn.name()} = time;
-
-      # LISTEN FOR CLIENT COMMANDS
-        whenever $conn.Supply(:bin) -> $incoming {
-          my $command = $interpreter.Normalize($incoming); #.decode('UTF-8').chomp.uc;
-          await $conn.write: $interpreter.Respond($command).encode();
-        }
-
-      # OR EMIT MESSAGES TO THE CLIENT(S)
-        $outgoing.tap(-> $message { 
-          my ($client, $out) = $message.split("|"); 
-          if ($client eq 'global') {
-            $conn.write: "GLOBAL -> $out\n".encode();
-          } elsif ($client eq $conn.name()) {
-            $conn.write: "PRIVATE -> $out\n".encode();
-          }
-        });
-      }
-    }
-  }
-# END CLIENT CONNECTIONS
+ END GAME LOOP
 
 
 
